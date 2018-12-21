08b566cfe439d1a5d9b7733bef8b00ec3296ce04
Andreas Menge <andreas.menge@moovel.com>
2018-12-21 11:29:46 +0100

    fix identation

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -3,25 +3,24 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const MovieSchema = new Schema({
-    Name: {
-      type: String,
-      required: 'Enter a first name'
-    }
-    // lastName: {
-    //     type: String,
-    //     required: 'Enter a first name'
-    // },
-    // email: {
-    //     type: String            
-    // },
-    // company: {
-    //     type: String            
-    // },
-    // phone: {
-    //     type: Number            
-    // },
-    // created_date: {
-    //     type: Date,
-    //     default: Date.now
-    // }
+  original_name: {
+    type: String,
+  },
+  imdb_id: {
+    type: String,
+    required: ''
+  },
+  release_date: {
+    type: String            
+  },
+  is_highlight: {
+    type: Boolean           
+  },
+  image_url: {
+    type: String           
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
 });
