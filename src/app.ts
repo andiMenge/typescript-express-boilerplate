939ad4a2b73cf031969bda0ec176774424070616
Andreas Menge <andi.menge@gmail.com>
2019-06-11 18:39:12 +0200

    refactor error handler

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,15 @@ import router from './router'
 
 export const app = express()
 
+// Custom error handler
+function errorHandler(err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).json({ error: err.message })
+}
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('tiny'))
 app.use('/', router)
 // Error handler goes last
-app.use(function(err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json({ error: err.message })
-})
+app.use(errorHandler)
