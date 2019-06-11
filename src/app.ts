a3f36e171b01250b029b3fdd6c6da881d8de7433
Andreas Menge <andi.menge@gmail.com>
2019-06-11 18:17:53 +0200

    refactor app.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,9 @@ import * as bodyParser from 'body-parser'
 import morgan from 'morgan'
 import router from './router'
 
-class App {
-  public app: express.Application
+export const app = express()
 
-  constructor() {
-    this.app = express()
-    this.config()
-  }
-
-  private config(): void {
-    this.app.use(bodyParser.json())
-    this.app.use(bodyParser.urlencoded({ extended: false }))
-    this.app.use(morgan('tiny')) // logger
-    this.app.use('/', router)
-  }
-}
-
-export default new App().app
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('tiny'))
+app.use('/', router)
