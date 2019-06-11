3d83dfce2427bab592a9987ac08d35119f617be7
Andreas Menge <andi.menge@gmail.com>
2019-06-11 18:11:23 +0200

    add comments

--- src/helloWorld/helloWordHandlers.ts
+++ src/helloWorld/helloWordHandlers.ts
@@ -3,7 +3,8 @@ import { helloWorld } from './helloWorld'
 
 const router = Router()
 
-router.get('/', (req, res) => {
+// path is /helloWorld/hello
+router.get('/hello', (req, res) => {
   const greeting: string = helloWorld()
   res.send({ msg: greeting })
 })
