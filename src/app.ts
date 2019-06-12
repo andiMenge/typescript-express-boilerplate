ff88e6c71cd6af8b401f7d3bf9ff8f4b07063317
Andreas Menge <andi.menge@gmail.com>
2019-06-12 10:30:31 +0200

    add default route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Request, Response, NextFunction } from 'express'
 import * as bodyParser from 'body-parser'
 import morgan from 'morgan'
 import router from './router'
@@ -7,9 +8,14 @@ import cors from 'cors'
 export const app = express()
 
 // Custom error handler
-function errorHandler(err, req, res, next) {
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   console.error(err.stack)
   res.status(500).json({ error: err.message })
+}
+
+// Default route
+function defaultRoute(req: Request, res: Response, next: NextFunction) {
+  res.sendStatus(404)
 }
 
 app.use(bodyParser.json())
@@ -17,5 +23,5 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('tiny'))
 app.use(cors())
 app.use('/', router)
-// Error handler goes last
-app.use(errorHandler)
+app.use(defaultRoute) // default route has to be last route
+app.use(errorHandler) // Error handler goes last
